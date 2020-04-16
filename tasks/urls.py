97ffeed1c97ef4a265ae79379343dea9ae021d8d
Joby Mathew <jobymathew.mec@gmail.com>
2020-04-17 02:20:36 +0530

    added functions

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'tasks'
+# app_name = 'tasks'
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.index,name='list'),
+    path('update_task/<str:pk>/',views.updateTask,name='update_task'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
-    return render(request,'tasks/list.html')
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request,'tasks/list.html',{'tasks':tasks, 'form':form})
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'tasks/update_task.html', {'form':form})
+
+    
